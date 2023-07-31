19fda1975cc847238c917561d253a49cf8ab06eb
Catalina <shagowofking1998@gmail.com>
2023-07-31 08:15:08 -0500

    🐛

--- menus/menu.php
+++ menus/menu.php
@@ -27,11 +27,11 @@ class Menu {
       <nav>
        <span> <iconify-icon icon="mingcute:menu-fill" class="icon" style="color: #bf7449;" width="1.5em"></iconify-icon></span>   
          <ul>
-         <li>Inicio</li>
-         <li>Lentes</li>
-         <li>Paños</li>
-         <li>Estuches</li>
-         <li>Liquidos</li>
+         <li><a href="index.html">Inicio</a></li>
+           <li><a href="./catalogo/lentes.html">Lentes</a></li>
+           <li><a href="pagina_panos.html">Paños</a></li>
+           <li><a href="./catalogo/estuches.html">Estuches</a></li>
+           <li><a href="./catalogo/liquidos.html">Liquidos</a></li>
        </ul> 
       </nav>    
 
@@ -127,11 +127,11 @@ class Menu {
       <nav>
        <span> <iconify-icon icon="mingcute:menu-fill" class="icon" style="color: #bf7449;" width="1.5em"></iconify-icon></span>   
          <ul>
-         <li>Inicio</li>
-         <li>Lentes</li>
-         <li>Paños</li>
-         <li>Estuches</li>
-         <li>Liquidos</li>
+         <li><a href="index.html">Inicio</a></li>
+         <li><a href="./catalogo/lentes.html">Lentes</a></li>
+         <li><a href="pagina_panos.html">Paños</a></li>
+         <li><a href="./catalogo/estuches.html">Estuches</a></li>
+         <li><a href="./catalogo/liquidos.html">Liquidos</a></li>
        </ul> 
       </nav>    
 
